12f88fa4065951e26e0443901bfe49c1dec5bc4a
hnascx <henrique.nascimento12@hotmail.com>
2025-09-04 15:03:03 -0300

    feat: Create basis structure of UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
+import "./styles/global.css"
+
 import { Provider as ReduxProvider } from "react-redux"
 
-import { AddTodo } from "./components/AddTodo"
-import { TodoList } from "./components/ToDoList"
+import { Player } from "./pages/Player"
 import { store } from "./store"
 
 export function App() {
   return (
-    <div style={{ backgroundColor: "black", color: "white", height: "100vh" }}>
-      <ReduxProvider store={store}>
-        <TodoList />
-        <AddTodo />
-      </ReduxProvider>
-    </div>
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
   )
 }
